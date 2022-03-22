cf2c540c44ec8888d373586bce61170267eecc5c
HearthR <hghss0603@gmail.com>
2022-03-23 00:09:36 +0900

    initial commit

---
+++ src/main/kotlin/me/hearthr/studymarathon/StudyMarathonApplication.kt
@@ -0,0 +1,11 @@
+package me.hearthr.studymarathon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyMarathonApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyMarathonApplication>(*args)
+}
---
+++ src/test/kotlin/me/hearthr/studymarathon/StudyMarathonApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.hearthr.studymarathon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyMarathonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
